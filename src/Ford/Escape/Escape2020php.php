a2383c6b91ee3ff61ff81c9251503beccc3689b8
[zhuliubao] <[zhuliubao@qie.tv]>
2020-05-08 23:34:47 +0800

    in order to build my packagist

---
+++ src/Ford/Escape/Escape2020php.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ford\Escape ;
+
+class Escape2020php
+{
+    public static function info()
+    {
+        echo "This is Ford Escape2016!<br />";
+    }
+}
---
+++ src/Ford/Fiesta/Fiesta2020.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Ford\Escape;
+
+
+class Fiesta2020
+{
+
+    public function info()
+    {
+        echo "This is Ford Fiesta2016!<br>";
+    }
+}
